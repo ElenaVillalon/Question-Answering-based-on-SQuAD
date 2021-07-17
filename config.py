91372f085bcb8e7af1907ea057d7d25402f49301
ElenaVillalon <54565692+ElenaVillalon@users.noreply.github.com>
2021-07-17 02:45:51 -0400

    Update config.py

--- config.py
+++ config.py
@@ -2,18 +2,18 @@
 exp = "exp-1"
 
 # data directories
-data_dir = "/Users/elizabethlorelei/Documents/Programming/Test/Question_Answ_Syst/SquAD/"
+data_dir = "/content/Question-Answering-based-on-SQuAD/SQuAD/"
 train_dir = data_dir + "train/"
 dev_dir = data_dir + "dev/"
 
 # model paths
-spacy_en = "/Users/elizabethlorelei/anaconda3/lib/python3.6/site-packages/en_core_web_sm/en_core_web_sm-2.1.0"
-glove = "/Users/elizabethlorelei/Documents/Programming/Test/Question_Answ_Syst/Data/glove.6B/" + "glove.6B.{}d.txt"
-squad_models = "output/" + exp
+spacy_en = "/usr/local/lib/python3.7/dist-packages/spacy/data/en"
+glove = "/content/glove.42B.300d.txt"
+squad_models = "/content/drive/MyDrive/ProyectoDeepLearningUC/output/" + exp
 
 # preprocessing values
 max_words = -1
-word_embedding_size = 100
+word_embedding_size = 300
 char_embedding_size = 8
 max_len_context = 400
 max_len_question = 50
@@ -21,12 +21,12 @@ max_len_word = 25
 
 # training hyper-parameters
 num_epochs = 1
-batch_size = 1
+batch_size = 32
 learning_rate = 0.5
 drop_prob = 0.2
 hidden_size = 100
 char_channel_width = 5
 char_channel_size = 100
-#cuda = True
-cuda = False
+cuda = True
+#cuda = False
 pretrained = False
